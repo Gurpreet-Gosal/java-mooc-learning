5217744e8dde46c9f17af381c905bae118820bba
Gurpreet Singh Gosal <er.gurpreetgosal@gmail.com>
2025-07-24 00:12:37 -0400

    Completed Day 1 exercises on Java basics

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,13 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Hello World");//when I use ln, it break the line
+        System.out.print(" from Gurpreet Singh");//when I don't use ln, it continues in same line
+        System.out.println(" Gosal");
+        System.out.print("next line using " + "\n");
+        System.out.print("hello\n");
+        System.out.println("...and the universe");
+    }
+}
---
+++ part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
@@ -0,0 +1,10 @@
+
+public class Dinosaur {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Once upon a time");//i used "sout" shortcut to get println code
+        System.out.println("there was");//i.e. I typed sout and then pressed tab button
+        System.out.println("a dinosaur");//System.out.println appears automatically
+    }
+}
