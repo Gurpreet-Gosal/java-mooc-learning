d7af457c4afa83b6fd50657a33fcb3cae2bf2e85
Gurpreet Singh Gosal <er.gurpreetgosal@gmail.com>
2025-07-24 13:03:15 -0400

    Completed exercises 18-36 on conditional statements

--- part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
+++ part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -5,6 +5,25 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Give points [0-100]:");
+        int pts = Integer.valueOf(scan.nextLine());
+        if(pts<0){
+            System.out.println("impossible!");
+        }else if(pts>=0&&pts<=49){
+            System.out.println("failed");
+        }else if (pts>=50&&pts<=59){
+            System.out.println("1");
+        }else if (pts>=60&&pts<=69){
+            System.out.println("2");
+        }else if (pts>=70&&pts<=79){
+            System.out.println("3");
+        }else if (pts>=80&&pts<=89){
+            System.out.println("4");
+        }else if (pts>=90&&pts<=100){
+            System.out.println("5");
+        }else if (pts>100){
+            System.out.println("incredible!");
+        }
 
     }
 }
--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,15 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Give a year:");
+        int yr = Integer.valueOf(scan.nextLine());
+        // A year is a leap year if it is divisible by 4,
+        // unless it is divisible by 100 but not by 400.
+        if ((yr % 4 == 0) && (yr % 100 != 0) || (yr % 400 == 0)) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
 
     }
 }
