6223f9d4c72b59c2e46882deb6848d060846156c
Gurpreet Singh Gosal <er.gurpreetgosal@gmail.com>
2025-07-30 12:09:22 -0400

    Completed exercises from 17-22 of Part 2

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,16 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //task is to ask user for input
+        System.out.println("Last number? ");
+        //task is to get input from user and convert it into Integer datatype
+        int num = Integer.valueOf(scanner.nextLine());
+        //task is to define a variable whose value will add in the sequence after each iteration
+        int sum = 0;
+        for (int i = 0; i <= num; i++) {
+            sum += i; // shorthand for sum = sum + i
+        }
+        System.out.println("The sum is " + sum);
 
     }
 }
--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,13 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number: ");
+        int num = Integer.valueOf(scanner.nextLine());
+        int fct = 1;
+        for (int i = 1; i <= num; i++) {
+            fct = fct * i;
+        }
+        System.out.println("Factorial: " + fct);
 
     }
 }
