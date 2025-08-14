9cf6060504b81d657c833b09193116cd6ea5bbea
Gurpreet Singh Gosal <er.gurpreetgosal@gmail.com>
2025-08-14 12:08:54 -0400

    Completed exercise 12-16 of Part4

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,35 @@
 
 public class Statistics {
+
+    private int count;
+    private int sum;
+
+    // The constructor initializes the variables
+    public Statistics() {
+        this.count = 0;
+        this.sum = 0;
+    }
+
+    // This method is called for each new number
+    public void addNumber(int number) {
+        this.count++;          // Increment the count of numbers
+        this.sum = this.sum + number; // Add the number to the sum
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public int sum() {
+        return this.sum;
+    }
+
+    public double average() {
+        // Avoid dividing by zero if no numbers have been added
+        if (this.count == 0) {
+            return 0;
+        }
+        // Cast the sum to a double to get an accurate decimal average
+        return (double) this.sum / this.count;
+    }
 }
