cc47ac829c6f8e38107a9cf09790ddff7ac2ff6e
Gurpreet Singh Gosal <er.gurpreetgosal@gmail.com>
2025-08-19 12:11:07 -0400

    Completed exercises 28,29,30 of Part4

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,18 +1,37 @@
 
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class NumbersFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("File? ");
+        System.out.println("File? ");
         String file = scanner.nextLine();
-        System.out.print("Lower bound? ");
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("");
+        System.out.println("Lower bound? ");
         int lowerBound = Integer.valueOf(scanner.nextLine());
-        System.out.print("Upper bound? ");
+        System.out.println("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        try ( Scanner readFile = new Scanner(Paths.get(file))) {
+            int count = 0;
+            // we read all the lines of the file
+            while (readFile.hasNextLine()) {
+                list.add(Integer.valueOf(readFile.nextLine()));
+            }
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) >= lowerBound && list.get(i) <= upperBound) {
+                    System.out.println(list.get(i));
+                    count++;
+                }
+            }
+            System.out.println("Numbers: " + count);
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
 
     }
 
