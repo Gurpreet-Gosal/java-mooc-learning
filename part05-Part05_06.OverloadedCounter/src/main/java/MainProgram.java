036fca964380a279fe8bd01fc07ca9c6285cc76c
Gurpreet Singh Gosal <er.gurpreetgosal@gmail.com>
2025-08-23 12:15:03 -0400

    Completed exercises 2-6 of Part5

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,18 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter counter = new Counter(2);
+        System.out.println(counter.getValue());
+        counter.increase();
+        System.out.println(counter.getValue());
+        counter.decrease();
+        System.out.println(counter.getValue());
+        counter.increase(2);
+        System.out.println(counter.getValue());
+        counter.decrease(5);
+        System.out.println(counter.getValue());
+        counter.decrease(4);
+        System.out.println(counter.getValue());
+
     }
 }
